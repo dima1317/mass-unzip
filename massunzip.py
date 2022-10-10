7872068600907320484726658e73e5c3678a1fb4
dima1317 <112827758+dima1317@users.noreply.github.com>
2022-10-10 07:41:47 +0000

    Update massunzip.py

--- massunzip.py
+++ massunzip.py
@@ -6,9 +6,8 @@ if sys.argv[1] == '':
   path = '/'
 else:
   path = sys.argv[1]
-#print(path)
+
 listOfFiles = os.listdir(path)
 for f in listOfFiles:
-  #print(path + '/' + f)
   with zipfile.ZipFile(path + '/' + f,'r') as zip_f:
     zip_f.extractall(sys.argv[2])
